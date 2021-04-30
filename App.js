597ed9da3e557b6b9eed15d63b2cf5f418492fa1
rishanis <rishanis@outlook.com>
2021-04-30 20:51:16 +0400

    app canvas

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView , Platform} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ backgroundColor: 'lightgreen', padding: 16}}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'lightblue', padding: 16 }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
