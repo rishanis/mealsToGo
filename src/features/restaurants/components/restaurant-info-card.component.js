f6616852d01228adc9a58c84701738edec442372
rishanis <rishanis@outlook.com>
2021-05-01 22:42:25 +0400

    star svg setup

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,28 +2,37 @@ import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const RestaurantCardCover = styled(Card.Cover)`
-  background-color: white;
+  padding: ${(props) => props.theme.space[2]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const Title = styled.Text`
-  padding: 10px;
-  font-size: 20px;
-  color: red;
-  font-weight: bold;
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Milan Vegies",
+    name = "Bubbles Falafel & Shawarma",
     icon,
     photos = [
       "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2011%2F12%2F13%2Franch-chicken-mac-cheese-sl-x.jpg",
     ],
-    address = "100, Muhaisnah 4, Dubai",
+    address = "100, Marine Drive, Colombo",
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily = false,
@@ -32,7 +41,11 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <SvgXml xml={star} width={20} height={20} />
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -6,15 +6,14 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: lightblue;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => (
