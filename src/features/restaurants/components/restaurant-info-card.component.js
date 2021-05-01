bacbeb72c5c01bea9118f5f189faf0e7354bcb94
rishanis <rishanis@outlook.com>
2021-05-01 17:12:41 +0400

    styled components added

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,10 +1,16 @@
 import React from "react";
+import styled from "styled-components/native";
 import { Text, StyleSheet, View } from "react-native";
 import { Card } from "react-native-paper";
 
+const Title = styled.Text`
+  padding: 15px;
+  font-size: 20px;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Milan Veg",
+    name = "Milan Vegies",
     icon,
     photos = [
       "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2011%2F12%2F13%2Franch-chicken-mac-cheese-sl-x.jpg",
@@ -17,8 +23,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <Card elevation={5} style={styles.card}>
       <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Text style={styles.title}>{name}</Text>
-      <Text style={styles.open}>{isOpenNow ? "Open" : "Closed"}</Text>
+      <Title>{name}</Title>
     </Card>
   );
 };
@@ -28,10 +33,10 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
   cover: {
-    padding: 20,
+    padding: 5,
     backgroundColor: "white",
   },
   title: {
-    padding: 20,
+    padding: 15,
   },
 });
